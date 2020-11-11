a14589d21b80ebbee882a1f9dac9f080d5a9f60f
Tomás Hernández <tomas.hernandez03@gmail.com>
2020-11-11 05:41:24 -0300

    fix: 🐛 Fixes an error on importing a constant

--- src/constants/validations.js
+++ src/constants/validations.js
@@ -1,1 +1,1 @@
-const BROWSER_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+export const BROWSER_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
